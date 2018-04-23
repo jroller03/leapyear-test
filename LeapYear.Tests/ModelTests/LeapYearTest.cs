60000ef489afef1f56e1440acb1b47ffb28a6ecd
Justin Roller <justin@>
2018-04-23 13:54:59 -0700

    initial commit with testing code.

---
+++ LeapYear.Tests/ModelTests/LeapYearTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeapYear;
+
+namespace LeapYear.Tests
+{
+  [TestClass]
+  public class LeapYearTest
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
